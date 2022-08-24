288755d1a394b0bd2de66875babf53dd20687ff7
Vladislav Mitrofanov <111977454+vlmf2803@users.noreply.github.com>
2022-08-24 16:03:36 +0300

    Add small delay to fix errors

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
+import os, time
 
 
 for i in range(0, 100):
-    os.system(f"wget -O temp/img-{i}.png https://thispersondoesnotexist.com/image")
+    os.system(f"wget -b -q -O temp/img-{i}.png https://thispersondoesnotexist.com/image")
+    print(i)
+    time.sleep(2)
